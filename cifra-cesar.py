7bc1739be760da843def626fb8f82c4256d60cb4
guleao <guztavusz@gmail.com>
2024-06-11 14:25:30 -0300

    Ajustes

--- cifra-cesar.py
+++ cifra-cesar.py
@@ -1,13 +1,26 @@
-message = "Hello, World"
+message = "Hello World"
 shift = 7
 
+LAST_CHAR_CODE = 90
+CHAR_RANGE = 26
+
+# Placeholder:
+result = ""
 
 #Looping que mostra as letras presentes dentro da message
-for char in message: 
-
+for char in message.upper(): 
+    if char.isalpha():
     # Converter caracteres para a ASCII.
-    char_code = ord(char)
-    print(char, char_code)
+        char_code = ord(char)
+        # Nova numeração de acordo com o ASCII + quaantidade de caracteres (shift).
+        new_char_code = char_code + shift
 
+        if new_char_code > LAST_CHAR_CODE:
+            new_char_code -= CHAR_RANGE
 
-    
+        new_char = chr(new_char_code)
+        result += new_char
+    else:
+        result += char 
+
+print(result)
