04de573b074a3b6c75407791e2185cdd14320445
guleao <guztavusz@gmail.com>
2024-06-11 14:13:43 -0300

    Inicio da produção do programa

---
+++ cifra-cesar.py
@@ -0,0 +1,13 @@
+message = "Hello, World"
+shift = 7
+
+
+#Looping que mostra as letras presentes dentro da message
+for char in message: 
+
+    # Converter caracteres para a ASCII.
+    char_code = ord(char)
+    print(char, char_code)
+
+
+    
