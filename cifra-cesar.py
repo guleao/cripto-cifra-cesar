c82a0379963deb19e175c7b4dc2220df7a00a32b
guleao <guztavusz@gmail.com>
2024-06-11 14:35:05 -0300

    Finalizado

--- cifra-cesar.py
+++ cifra-cesar.py
@@ -1,26 +1,34 @@
-message = "Hello World"
-shift = 7
+LAST_CHAR_CODE = ord("A")
+FIRST_CHAR_CODE = ord("Z")
+CHAR_RANGE = LAST_CHAR_CODE - FIRST_CHAR_CODE + 1
 
-LAST_CHAR_CODE = 90
-CHAR_RANGE = 26
 
-# Placeholder:
-result = ""
+def cesar_shift(message, shift):
+    # Placeholder:
+    result = ""
 
-#Looping que mostra as letras presentes dentro da message
-for char in message.upper(): 
-    if char.isalpha():
-    # Converter caracteres para a ASCII.
-        char_code = ord(char)
-        # Nova numeração de acordo com o ASCII + quaantidade de caracteres (shift).
-        new_char_code = char_code + shift
+    #Looping que mostra as letras presentes dentro da message
+    for char in message.upper(): 
+        if char.isalpha():
+        # Converter caracteres para a ASCII.
+            char_code = ord(char)
+            # Nova numeração de acordo com o ASCII + quaantidade de caracteres (shift).
+            new_char_code = char_code + shift
 
-        if new_char_code > LAST_CHAR_CODE:
-            new_char_code -= CHAR_RANGE
+            if new_char_code > LAST_CHAR_CODE:
+                new_char_code -= CHAR_RANGE
 
-        new_char = chr(new_char_code)
-        result += new_char
-    else:
-        result += char 
+            if new_char_code < FIRST_CHAR_CODE:
+                new_char_code += CHAR_RANGE
 
-print(result)
+            new_char = chr(new_char_code)
+            result += new_char
+        else:
+            result += char 
+
+    print(result)
+
+user_message = input("Message to Encrypt: ")
+user_shift_key = int(input("Shift key:"))
+
+cesar_shift(user_message, user_shift_key)
